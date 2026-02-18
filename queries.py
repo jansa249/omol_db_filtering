611ec26228b75680f917fb82ab17b5b63eea570f
Jan Salomon <janwsalomon@gmail.com>
2026-02-18 14:17:58 +0100

    Improved query with primary amines

--- queries.py
+++ queries.py
@@ -1,8 +1,8 @@
 QUERY_DICT = {
-    'adenine' : 'N1~C~N~C2~C1~N~C~N~C2~N',
+    'adenine' : 'N1~C~N~C2~C1~N~C~N~C2~[NH2]',
     'guanine' : 'N1~C~N~C2~C1~N~C(~N)~N~C2~O',
     'uracthym' : 'N1~C~C~C(~O)~N~C1(~O)',
-    'cytosine' : 'N1~C~C~C(~N)~N~C1(~O)',
+    'cytosine' : 'N1~C~C~C(~[NH2])~N~C1(~O)',
     'dribose_n' : 'O~C~C1~C(~O)~C~C(~N)~O1',
     'dribose' : 'C~C1~C(~O)~C~C~O1',
     'phosphate' : 'P(~O)(~O)(~O)(~O)',
