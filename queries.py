ee511dbc10c57f0175356ae47732bd4a4db52336
Jan Salomon <janwsalomon@gmail.com>
2026-02-22 19:12:13 +0100

    Added explanation

--- queries.py
+++ queries.py
@@ -1,12 +1,16 @@
+"""
+Dictionary for SMARTS queries for interesting molecule substructures.
+Every entry here is used by other scripts.
+Custom entries can be added after 'phosphate'.
+"""
 QUERY_DICT = {
-    'adenine' : 'N1~C~N~C2~C1~N~C~N~C2~[NH2]',
-    'guanine' : 'N1~C~N~C2~C1~N~C(~N)~N~C2~O',
-    'uracthym' : 'N1~C~C~C(~O)~N~C1(~O)',
-    'cytosine' : 'N1~C~C~C(~[NH2])~N~C1(~O)',
-    'dribose_n' : 'O~C~C1~C(~O)~C~C(~N)~O1',
-    'dribose' : 'C~C1~C(~O)~C~C~O1',
-    'ribose' : 'C~C1~C(~O)~C(~O)~C~O1',
-    'phosphate' : 'P(~O)(~O)(~O)(~O)',
-    # 'phosphate' : 'P(~O)(~O)(~O)',
+    'adenine' : 'N1~C~N~C2~C1~N~C~N~C2~[NH2]',  # mandatory
+    'guanine' : 'N1~C~N~C2~C1~N~C(~N)~N~C2~O',  # mandatory
+    'uracthym' : 'N1~C~C~C(~O)~N~C1(~O)',       # mandatory
+    'cytosine' : 'N1~C~C~C(~[NH2])~N~C1(~O)',   # mandatory
+    'dribose_n' : 'O~C~C1~C(~O)~C~C(~N)~O1',    # mandatory
+    'dribose' : 'C~C1~C(~O)~C~C~O1',            # mandatory
+    'ribose' : 'C~C1~C(~O)~C(~O)~C~O1',         # mandatory
+    'phosphate' : 'P(~O)(~O)(~O)(~O)',          # mandatory
 }
 
